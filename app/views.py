b1edb05378477ca6261feacddf91eef23defa0c8
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2016-08-05 23:43:05 +0200

    2016-08-05 configuration test, favicon

--- app/views.py
+++ app/views.py
@@ -1,7 +1,16 @@
 from app import app
+import os
+from flask import send_from_directory
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, microblog app!"
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico',
+                               mimetype='image/vnd.microsoft.icon')
--- microblog.py
+++ microblog.py
@@ -1,4 +1,6 @@
+import os
 from flask import Flask
+from flask import send_from_directory
 
 app = Flask(__name__)
 
@@ -7,6 +9,12 @@ app = Flask(__name__)
 def index():
     return 'Welcome to Flask app home page!'
 
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico',
+                               mimetype='image/vnd.microsoft.icon')
 
 if __name__ == '__main__':
     app.run()
